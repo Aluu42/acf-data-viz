23fb92c381431db621a702c87d3cc3ccbc796e38
Abby Krishnan <abbykrishnan16@gmail.com>
2019-04-12 18:24:41 -0500

    heat map with points

--- app.py
+++ app.py
@@ -14,36 +14,91 @@ from bokeh.sampledata import us_states
 from bokeh.plotting import *
 from bokeh.embed import components
 from bokeh.resources import INLINE
-
+from bokeh.models import *
+from us_state_abbrev import *
 
 us_states = us_states.data.copy()
 del us_states["HI"]
 del us_states["AK"]
-scholarships = pd.read_csv("data.csv")
-scholarships.fillna(0)
+us_states = dict([(us_state_abbrev[key], value) for key, value in us_states.items()])
 
 app = Flask(__name__)
+def cleanScholarshipData():
+
+    scholarships = pd.read_csv("data_with_states.csv")
+    scholarships.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
+    top = 49.3457868 # north lat
+    left = -124.7844079 # west long
+    right = -66.9513812 # east long
+    bottom =  24.7433195 # south lat
+
+    scholarships['Longitude'] = scholarships['Longitude'].apply(lambda x: -1*float(str(x).split(' ')[0]))
+    scholarships['Latitude'] = scholarships['Latitude'].apply(lambda x: float(str(x).split(' ')[0]))
+    scholarships['GrantAmt'] = scholarships['GrantAmt'].apply(lambda x: float(str(x)[1:].replace(',', '')))
+
+
+    scholarships = scholarships[scholarships['Longitude'] > left]
+    scholarships = scholarships[scholarships['Longitude'] < right]
+    scholarships = scholarships[scholarships['Latitude'] > bottom]
+    scholarships = scholarships[scholarships['Latitude'] < top]
+
+    scholarshipsAgg = scholarships.groupby(['Institution', 'Latitude', 'Longitude', 'State'])[["GrantAmt"]].sum().reset_index()
+
+    return scholarshipsAgg
+
 
 def generateMap():
 
     state_xs = [us_states[code]["lons"] for code in us_states]
     state_ys = [us_states[code]["lats"] for code in us_states]
 
-    p = figure(title="Plotting Schools",
-               toolbar_location="left", plot_width=1100, plot_height=700)
+    scholarships = cleanScholarshipData()
+    stateTotals = scholarships.groupby(['State'])[["GrantAmt"]].sum().reset_index()
 
-    p.patches(state_xs, state_ys, fill_alpha=0.0,
+    source = ColumnDataSource(data=dict(
+        x=scholarships['Longitude'].values,
+        y=scholarships['Latitude'].values,
+        school=scholarships['Institution'],
+        grantAmt = scholarships['GrantAmt']
+
+    ))
+
+
+    p = figure(title="Plotting Schools",
+               toolbar_location="left", plot_width=1000, plot_height=750)
+
+
+    color_palette = ['#abcafc', '#5a92ed', '#3468bc']
+    state_colors = []
+    for us_state in us_states:
+        try:
+
+            totalMoney = stateTotals.loc[stateTotals['State'] == us_state].values[0][1]
+        except:
+            totalMoney = 0
+
+        if totalMoney in range(5000, 20000):
+            state_colors.append(color_palette[1])
+        elif totalMoney > 20000:
+            state_colors.append(color_palette[2])
+        else:
+            state_colors.append(color_palette[0])
+
+
+
+
+    p.patches(state_xs, state_ys, fill_alpha=1, fill_color=state_colors,
         line_color="#884444", line_width=1.5)
 
-    scholarships['Longitude'] = scholarships['Longitude'].apply(lambda x: -1*float(str(x).split(' ')[0]))
-    scholarships['Latitude'] = scholarships['Latitude'].apply(lambda x: float(str(x).split(' ')[0]))
 
-    x = scholarships['Longitude'].values
-    y = scholarships['Latitude'].values
+    p.circle('x', 'y', source=source, size=2, color='navy', alpha=1)
+    g1 = Cross(x='x', y='y')
+    g1_r = p.add_glyph(source_or_glyph=source, glyph=g1)
+    g1_hover = HoverTool(renderers=[g1_r],
+                             tooltips=[('School', '@school'), ('GrantAmt', '@grantAmt')])
+    p.add_tools(g1_hover)
 
-    p.circle(x, y, size=8, color='navy', alpha=1)
-    # output_file("map.html")
-    # show(p)
+    show(p)
 
     return p
 
@@ -60,4 +115,4 @@ def default():
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, threaded=True)
